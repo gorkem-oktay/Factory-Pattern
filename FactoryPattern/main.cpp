7e2b31d03678b725802975d3ac895e8ae21ac955
Görkem Oktay <gorkem.oktay@masomo.com>
2019-02-20 15:16:35 +0300

    Initial Commit

---
+++ FactoryPattern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FactoryPattern
+//
+//  Created by Görkem Oktay on 20.02.2019.
+//  Copyright © 2019 Görkem Oktay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
