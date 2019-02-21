51f242c922e685e41ee089de32f7c552b2ee1a39
Görkem Oktay <gorkem.oktay@masomo.com>
2019-02-21 17:09:39 +0300

    Implemented pattern

---
+++ FactoryPattern/Characters/Headers.hpp
@@ -0,0 +1,17 @@
+//
+//  Headers.hpp
+//  FactoryPattern
+//
+//  Created by Görkem Oktay on 21.02.2019.
+//  Copyright © 2019 Görkem Oktay. All rights reserved.
+//
+
+#ifndef Headers_hpp
+#define Headers_hpp
+
+#include "ICharacter.hpp"
+#include "Knight.hpp"
+#include "Goblin.hpp"
+#include "Troll.hpp"
+
+#endif /* Headers_hpp */
